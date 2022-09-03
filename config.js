03de962f1d5f18dae1127bdf3503ceeadcb2291c
Astesik <jakub.sikora30@wp.pl>
2022-09-03 18:53:32 +0200

    config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    db: '',
+    db: 'mongodb+srv://admin:SXriHtnwcuSQBjFr@cluster0.fsomgm3.mongodb.net/?retryWrites=true&w=majority',
     keySession: ['TWOJKLUCZ'],
     maxAgeSession: 24 * 60 * 60 * 1000
 }
